e65d3a1003ce6055bfd464ec2d85ee285791f576
aartiwadagaonkar <106589221+aartiwadagaonkar@users.noreply.github.com>
2023-01-31 17:06:36 +0530

    scripts add

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ db()
 const PORT = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
+app.use(express.static("public"))
 app.use(cors())
 // app.use(cors({
 //     origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
